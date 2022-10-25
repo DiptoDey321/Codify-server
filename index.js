71c7e7ccd607655b408aa87d323205b795ac98d5
Dipto Dey <diptodey2005@gmail.com>
2022-10-26 00:17:58 +0600

    cors installed and utilized

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.port || 5000 ;
 
 const courses =  require('./data/courses.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello From codify!')
