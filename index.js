61a6a10ba1af55e3446a463c37cdf90c883da26d
Dipto Dey <diptodey2005@gmail.com>
2022-10-26 00:02:20 +0600

    course file added

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 5000 ;
 
+const courses =  require('./data/courses.json')
+
 app.get('/', (req, res) => {
     res.send('Hello From codify!')
+})
+
+app.get('/courses', (req, res) =>{
+    res.send(courses)
 })
 
 app.listen(port, () => {
